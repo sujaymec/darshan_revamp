1e1ed6eabf8461b90b93fa50fe5a581ee4282835
sujaymec <sujaymec@gmail.com>
2024-09-30 19:42:34 +0530

    adding assets

--- src/App.js
+++ src/App.js
@@ -8,34 +8,49 @@ import {
   Nav,
   NavItem,
   NavLink,
+  UncontrolledDropdown,
+  DropdownToggle,
+  DropdownMenu,
+  DropdownItem,
+  // NavbarText,
 } from 'reactstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
+import './style.css';
 
-function App() {
-  const [collapsed, setCollapsed] = useState(true);
+function App(args) {
+  const [isOpen, setIsOpen] = useState(false);
 
-  const toggleNavbar = () => setCollapsed(!collapsed);
-
+  const toggle = () => setIsOpen(!isOpen);
 
   return (
-    <div className="App">
-      <Navbar color="faded" light>
-        <NavbarBrand href="/" className="me-auto">
-          reactstrap
-        </NavbarBrand>
-        <NavbarToggler onClick={toggleNavbar} className="me-2" />
-        <Collapse isOpen={!collapsed} navbar>
-          <Nav navbar>
+    <div className='darshan-container'>
+      <Navbar {...args} className="navbar-expand-md">
+        <NavbarBrand href="/">Darshan</NavbarBrand>
+        <NavbarToggler onClick={toggle} />
+        <Collapse isOpen={isOpen} navbar>
+          <Nav className="me-auto" navbar>
             <NavItem>
-              <NavLink href="/components/">Components</NavLink>
+              <NavLink href="/components/">About</NavLink>
             </NavItem>
             <NavItem>
               <NavLink href="https://github.com/reactstrap/reactstrap">
-                GitHub
+                Downloads
               </NavLink>
             </NavItem>
+            <UncontrolledDropdown nav inNavbar>
+              <DropdownToggle nav caret>
+                Disciplines
+              </DropdownToggle>
+              <DropdownMenu right>
+                <DropdownItem>GaanaPurna</DropdownItem>
+                <DropdownItem>ChitraPurna</DropdownItem>
+                <DropdownItem divider />
+                <DropdownItem>VadyaPurna</DropdownItem>
+              </DropdownMenu>
+            </UncontrolledDropdown>
           </Nav>
+          {/* <NavbarText>Simple Text</NavbarText> */}
         </Collapse>
       </Navbar>
     </div>
