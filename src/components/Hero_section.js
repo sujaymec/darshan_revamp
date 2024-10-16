f5d963a2362a1fe06b037975e1ea272643dfb770
sujaymec <sujaymec@gmail.com>
2024-10-16 22:24:44 +0530

    changes for deployment

--- src/components/Hero_section.js
+++ src/components/Hero_section.js
@@ -3,8 +3,9 @@ import React from "react";
 function HeroSection () {
     return (
         <div className="hero-section">
-            <h1>Educational and Charitable Institution</h1>
             <img src="https://i.imgur.com/NTfitFP.png" alt="sth" />
+            <h2>Educational and Charitable Institution</h2>
+            <h3>Master Franchisee BSS Cultural Mission National Development Agency, Established in 1952 by Planning Commission, Government of India</h3>
         </div>
     )
 }
