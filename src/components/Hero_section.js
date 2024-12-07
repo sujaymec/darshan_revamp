d8a879ae075b82229444ad2b612ec01a52f7a1e0
sujaymec <sujaymec@gmail.com>
2024-12-08 00:09:46 +0530

    many changes

--- src/components/Hero_section.js
+++ src/components/Hero_section.js
@@ -1,11 +1,21 @@
 import React from "react";
+import { Button } from "reactstrap";
 
 function HeroSection () {
     return (
         <div className="hero-section">
-            <img src="https://i.imgur.com/NTfitFP.png" alt="sth" />
-            <h2>Educational and Charitable Institution</h2>
-            <h3>Master Franchisee BSS Cultural Mission National Development Agency, Established in 1952 by Planning Commission, Government of India</h3>
+            <h1>Darshan</h1>
+            <div className="hero-text">
+                <h3>
+                    {/* <span>Educational and Charitable Organization</span><br /> */}
+                    Master Franchisee BSS Cultural Mission National Development Agency
+                </h3>
+                <img src="https://i.imgur.com/NTfitFP.png" alt="sth" />
+            </div>
+            <div className="hero-buttons">
+                <Button color="info">Downloads</Button>
+                <Button color="danger">Brochure</Button>
+            </div>
         </div>
     )
 }
